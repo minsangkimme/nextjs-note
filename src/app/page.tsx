b98d6c65c847a840362112e72976383322624873
minsangkimme <minsang.tech@gmail.com>
2023-11-20 22:33:12 +0900

    fix: ver2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-import Image from 'next/image';
-import { Inter } from 'next/font/google';
-import styles from './page.module.css';
-import Counter from '@/components/counter';
-const inter = Inter({ subsets: ['latin'] });
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "./page.module.css";
+import Counter from "@/components/counter";
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-      <h1>홈페이지다!!</h1>
+      <h1>홈페이지다!!ver2</h1>
       <Counter />
     </>
   );
