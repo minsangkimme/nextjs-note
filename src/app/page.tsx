7baecb95c75cbcb2d418b381cbe1b841c4214baf
minsangkimme <minsang.tech@gmail.com>
2023-11-20 22:36:58 +0900

    fix: ver4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <>
-      <h1>홈페이지다!!ver2</h1>
+      <h1>홈페이지다!!ver4</h1>
       <Counter />
     </>
   );
